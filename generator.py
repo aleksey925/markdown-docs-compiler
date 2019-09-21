cce451338ea266a1f162003f80196994d4e05c84
Aleksey Petrunnik <>
2019-09-21 15:55:43 +0400

    Доработана обработка ошибок при извлечении изменений из реппозитория

--- generator.py
+++ generator.py
@@ -21,20 +21,24 @@ class GitProgress(git.remote.RemoteProgress):
 
 
 def pull_repo(repo_url, source_dir: str, pull_error: bool = False):
-    if pull_error:
-        shutil.rmtree(source_dir, ignore_errors=True)
-
     if exists(source_dir) and len(os.listdir(source_dir)) == 0:
         print('Клонирование реппозитория:')
         git.Repo.clone_from(repo_url, source_dir, progress=GitProgress())
         print()
+    elif pull_error is True:
+        exit(1)
     else:
         try:
-            print('Извлечение изменений реппозитория:')
+            print('Извлекаем изменения из реппозитория:')
             git_repo = git.Repo(source_dir)
             git_repo.remotes.origin.pull(progress=GitProgress())
+            print()
         except Exception:
+            print('Возникла ошибка при попытке выполнить git pull')
+            clear_dir(source_dir)
             pull_repo(repo_url, source_dir, True)
+
+    print('Извлечение изменений завершено')
 
 
 def clear_dir(path: str):
